5e645240d5d25329e71562c927b112640ab413f2
Ajna Nayak <your-github-email@gmail.com>
2026-03-26 18:48:58 +0530

    updated now

--- login.py
+++ login.py
@@ -1,9 +1,19 @@
-print("Login System")
+print(" Login System ")
 
+# correct details
+correct_username = "admin"
+correct_password = "123"
+
+# user input
 username = input("Enter username: ")
 password = input("Enter password: ")
 
-if username == "admin" and password == "123":
+# checking
+if username == correct_username and password == correct_password:
     print("Login successful!")
+    print("Welcome", username)
 else:
-    print("Login failed!")
+    print("Wrong username or password")
+    print("Access denied")
+
+print("Program ended")
