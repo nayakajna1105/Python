4a43d86d0e40cdfb07e5057a9b00b5d5d3efaa43
Ajna Nayak <your-github-email@gmail.com>
2026-05-29 18:30:56 +0530

    updated

---
+++ evenodd.py
@@ -0,0 +1,10 @@
+print("Even or Odd Checker")
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("The number is Even")
+else:
+    print("The number is Odd")
+
+print("Thank you!")
